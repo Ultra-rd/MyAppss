acd79f3c5bf6273d7b4c5b28dcea7ead7212e266
Ultra-rd <67000715+Ultra-rd@users.noreply.github.com>
2022-05-28 19:57:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapps/UserListActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapps;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserListActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_list);
+        textView = findViewById(R.id.textView);
+        String emailfromIntent = getIntent().getStringExtra("EMAIL");
+        textView.setText(emailfromIntent);
+    }
+
+    public void  startStu(View v){
+        Intent intent = new Intent(this, stu.class);
+        startActivity(intent);
+    }
+
+    public void startPre(View v){
+        Intent intent2 = new Intent(this,preActivity.class);
+        startActivity(intent2);
+    }
+}
